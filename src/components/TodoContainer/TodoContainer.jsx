4b14125f45bcc71b5c5fa1f6b1f13d8f5c8fa49f
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2024-09-13 10:12:07 +0600

    Delete issue solve

--- src/components/TodoContainer/TodoContainer.jsx
+++ src/components/TodoContainer/TodoContainer.jsx
@@ -3,6 +3,7 @@ import AddTodoModal from "../AddTodoModal/AddTodoModal";
 import TodoFilter from "../TodoFilter";
 import "./TodoContainer.css";
 import TodoCard from "../TodoCard";
+import { key } from "localforage";
 
 const TodoContainer = () => {
   const [tasks, setTasks] = useState([]);
@@ -15,9 +16,11 @@ const TodoContainer = () => {
   const handleDelete = (deleteId) => {
     console.log(deleteId);
 
-    const remainTasks = tasks?.filter((task) => task?.id != deleteId)
+    const remainTasks = tasks?.filter((task) => task?.id != deleteId);
     console.log(remainTasks);
-    setTasks(remainTasks)
+    setTasks(remainTasks);
+
+    localStorage.setItem("tasks", JSON.stringify(remainTasks));
   };
 
   const handleEdit = (editId) => {
