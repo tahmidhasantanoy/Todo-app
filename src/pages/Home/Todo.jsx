b9bc76288123b04f936a92e6c80b8903097159e8
tahmidhasantanoy <tahmid15-12920@diu.edu.bd>
2024-09-14 19:38:20 +0600

    Framer motion added in text

--- src/pages/Home/Todo.jsx
+++ src/pages/Home/Todo.jsx
@@ -1,12 +1,25 @@
 import { Outlet } from "react-router-dom";
+import { motion } from "framer-motion";
 import Container from "../../components/Container";
 
 const Todo = () => {
   return (
     <Container>
-      <h3 className="text-center font-bold text-3xl my-14">My Todos </h3>
-      <Outlet>
-      </Outlet>
+      <motion.h3
+        className="text-center font-bold text-3xl my-14"
+        initial={{ scale: 0.8 }}  
+        animate={{ scale: [1.2, 0.9, 1] }}  
+        transition={{
+          duration: 0.8,  
+          ease: "easeInOut", 
+          times: [0, 0.2, 1],  
+          repeat: 8,  
+          repeatType: "reverse",
+        }}
+      >
+        My Todos
+      </motion.h3>
+      <Outlet />
     </Container>
   );
 };
